298f4aefaf187653ba6709f9b40ef0c8c809f3fb
irisjitomo <irisjitomo@eyahoo.com>
2019-10-08 18:31:39 -0400

    update and delte

--- router/car-dealer-router.js
+++ router/car-dealer-router.js
@@ -16,6 +16,17 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    db('cars').where('id', '=', req.params.id).first()
+    .then(car => {
+        !car ? res.status(404).json({ error: 'ID does not exist' }) :
+        res.json(car)
+    })
+    .catch(() => {
+        res.status(500).json({ message: "Failed to get data" })
+    })
+})
+
 router.post('/', (req, res) => {
     !req.body.VIN || !req.body.Make || !req.body.Model || !req.body.Mileage 
     ? res.status(400).json({ required: 'VIN, Make, Model, Mileage' })
@@ -28,6 +39,30 @@ router.post('/', (req, res) => {
     })
 })
 
+router.delete('/:id', (req, res) => {
+    db('cars').where('id', '=', req.params.id)
+    .del()
+    .then(car => {
+        !car ? res.status(404).json({ error: 'ID does not exist' }) :
+        res.json(car)
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'could not delete' })
+    })
+})
+
+router.put('/:id', (req, res) => {
+    !req.body.VIN || !req.body.Make || !req.body.Model || !req.body.Mileage 
+    ? res.status(400).json({ required: 'VIN, Make, Model, Mileage' })
+    : db('cars').where('id', '=', req.params.id).update(req.body)
+    .then(car => {
+        !car ? res.status(404).json({ error: 'ID does not exist' }) :
+        res.status(200).json(car)
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+})
 
 
 module.exports = router;
